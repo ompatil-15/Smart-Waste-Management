2aea483a4f5937d37dd00a95be0fd29be3481013
Om Patil <patilom001@gmail.com>
2024-04-04 12:10:39 +0530

    removed unused packages

--- backend-swm/config/database.js
+++ backend-swm/config/database.js
@@ -2,7 +2,6 @@ let server = require('./server');
 const path = require('path')
 const mongoose = require('mongoose')
 const express = require('express');
-const methodOverride = require('method-override')
 
 
 mongoose.connect('mongodb://localhost:27017/swm')
@@ -17,7 +16,6 @@ mongoose.connect('mongodb://localhost:27017/swm')
 server.set('views', path.join(__dirname, 'views'));
 server.set('view engine', 'ejs');
 server.use(express.urlencoded({extended: true}))
-server.use(methodOverride('_method'))
 
 
 module.exports = server;
